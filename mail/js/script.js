b28f6c2277f9522df48a1a484a899057a3bae673
KevinSonda <kevinsonda@gmail.com>
2024-07-04 01:53:44 +0200

    [UPD]recupero il valore del input

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,3 +3,12 @@ let email = ['kevinsonda@gmail.com', 'giacomo@gmail.com', 'giuseppe@gmail.com']
 
 //recupero il pulsante dal dom
 const btn = document.getElementById('check')
+
+//aggiungo l'evento click al pulsante
+btn.addEventListener('click', function(){
+    
+    //recupero il vaolre del campo input
+    let user_email = document.getElementById('email').value;
+
+    
+})
