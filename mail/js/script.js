4760f8ae080cffe44e88d65a9ffb27e5f1e384bc
KevinSonda <kevinsonda@gmail.com>
2024-07-04 01:57:05 +0200

    [UPD] ciclo l'array e confronto mail inserita dall'utente

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,5 +10,11 @@ btn.addEventListener('click', function(){
     //recupero il vaolre del campo input
     let user_email = document.getElementById('email').value;
 
-    
+    //ciclo l'array
+    for (let i = 0; i < email.length; i++){
+        //confronto la mail inserita dall'utente con quelle attualmente nell'array
+        if (emails[i] == user_email) {
+            console.log('Benvenuto')
+        }
+    }
 })
