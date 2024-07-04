cd6946f01ddcae74698ca3ddbe0bc80dc3946a74
KevinSonda <kevinsonda@gmail.com>
2024-07-04 02:18:36 +0200

    [UPD]Confronto i nnumeri

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,3 +3,14 @@ let user_number = Math.floor(Math.random() * 6 + 1);
 
 //genero il numero random per il pc
 let pc_number = Math.floor(Math.random() * 6 + 1);
+
+//confronto i due numeri
+if (user_number > pc_number){
+    console.log('Hai vinto');
+}
+else if (user_number < pc_number){
+    console.log('Hai perso');
+}
+else {
+    console.log('Pareggio')
+}
