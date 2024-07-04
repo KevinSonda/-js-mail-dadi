7f2eff34be79e35e140a351e89c20be465c10292
KevinSonda <kevinsonda@gmail.com>
2024-07-04 02:11:24 +0200

    [UPD]Creazione file html, js e README.d per es dadi

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,11 +10,21 @@ btn.addEventListener('click', function(){
     //recupero il vaolre del campo input
     let user_email = document.getElementById('email').value;
 
+    //definisco una variabile flag per il cotrollo del email
+    let check_mail = false;
+
     //ciclo l'array
     for (let i = 0; i < email.length; i++){
         //confronto la mail inserita dall'utente con quelle attualmente nell'array
         if (emails[i] == user_email) {
-            console.log('Benvenuto')
+            check_mail = true;
         }
+    }
+
+    if(check_mail) {
+        console.log('Mail trovata. Benvenuto')
+    }
+    else {
+        console.log('Mail non trovata. Chi sei?')
     }
 })
