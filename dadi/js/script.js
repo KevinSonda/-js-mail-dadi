6f86d75f7e59922d648c6f4fac32fa885417376f
KevinSonda <kevinsonda@gmail.com>
2024-07-04 02:16:02 +0200

    [UPD]Genero i numeri per il pc e l'utente

---
+++ dadi/js/script.js
@@ -0,0 +1,5 @@
+//genero il numero random per l'utente
+let user_number = Math.floor(Math.random() * 6 + 1);
+
+//genero il numero random per il pc
+let pc_number = Math.floor(Math.random() * 6 + 1);
